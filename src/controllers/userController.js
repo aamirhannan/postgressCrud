582470adf819fce742496644cd3333fa56e4e3d6
aamirhannan <aamirhannan08@gmail.com>
2024-12-30 17:04:31 +0530

    code removed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-import { createUserService, deleteUserService, getAllUsersService, getUserByEmailService, getUserByIdService, updateUserService } from "../models/userModel.js";
+import { createUserService, getUserByEmailService } from "../models/userModel.js";
 
 // standard response hadler
 const handleResponse = (res, status, message, data = null) => {
@@ -32,62 +32,6 @@ export const handleUserSignin = async (req, res, next) => {
             return handleResponse(res, 401, "SIGNIN_FAILED");
         }
         handleResponse(res, 200, "SIGNIN_SUCCESS", user);
-    } catch (error) {
-        next(error);
-    }
-};
-
-// get all users controller
-export const getAllUsers = async (req, res, next) => {
-    try {
-        const users = await getAllUsersService();
-        users.forEach(user => {
-            delete user.password;
-        });
-        handleResponse(res, 200, "USERS_FETCHED_SUCCESS", users);
-    } catch (error) {
-        next(error);
-    }
-};
-
-// get user by id controller
-export const getUserById = async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        const user = await getUserByIdService(id);
-        if (!user) {
-            return handleResponse(res, 404, "USER_NOT_FOUND");
-        }
-        handleResponse(res, 200, "USER_FETCHED_SUCCESS", user);
-    } catch (error) {
-        next(error);
-    }
-};
-
-// update user controller
-export const updateUser = async (req, res, next) => {
-    const { id } = req.params;
-    const { name, email } = req.body;
-    try {
-        const updatedUser = await updateUserService(id, name, email);
-        if (!updatedUser) {
-            return handleResponse(res, 404, "USER_NOT_FOUND");
-        }
-        handleResponse(res, 200, "USER_UPDATED_SUCCESS", updatedUser);
-    } catch (error) {
-        next(error);
-    }
-};
-
-// delete user controller
-export const deleteUser = async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        const deletedUser = await deleteUserService(id);
-        if (!deletedUser) {
-            return handleResponse(res, 404, "USER_NOT_FOUND");
-        }
-        handleResponse(res, 200, "USER_DELETED_SUCCESS", deletedUser);
     } catch (error) {
         next(error);
     }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, getAllUsers, getUserById, updateUser, deleteUser, handleUserSignin } from "../controllers/userController.js";
+import { createUser, handleUserSignin } from "../controllers/userController.js";
 import { validateUser, validateSignin } from "../middleware/inputValidator.js";
 const router = express.Router();
 
@@ -7,9 +7,5 @@ const router = express.Router();
 
 router.post("/signup", validateUser, createUser);
 router.post("/signin", validateSignin, handleUserSignin);
-router.get("/users", getAllUsers);
-router.get("/users/:id", getUserById);
-router.put("/users/:id", validateUser, updateUser);
-router.delete("/users/:id", deleteUser);
 
 export default router;
